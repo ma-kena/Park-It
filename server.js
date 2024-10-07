4b8e735651da0beb1fb5c663593315bbcb88acf0
ma-kena <79266892+ma-kena@users.noreply.github.com>
2024-10-07 17:47:03 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+document.getElementById('homeBtn').addEventListener('click', function() {
+    alert('Home button clicked!');
+});
+
+document.getElementById('loginBtn').addEventListener('click', function() {
+    alert('Login button clicked!');
+});
+
+document.getElementById('signupBtn').addEventListener('click', function() {
+    alert('Sign Up button clicked!');
+});
+
+
+
+
+
+
+
